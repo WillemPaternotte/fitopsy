1648b297c506eb13c6f620960a5e9a177b955921
WillemPaternotte <51127114+WillemPaternotte@users.noreply.github.com>
2021-05-28 16:12:04 +0200

    push push push

---
+++ GUI_fitopsy.py
@@ -0,0 +1,47 @@
+# Vul hier de naam van je programma in:
+#fitopsy
+#
+# Vul hier jullie namen in:
+# Willem Paternotte en Robin KUijpers
+#
+#
+###------------------bibliotheek en globale variabelen--------------
+from tkinter import *
+from tkinter.font import Font
+from pygame import mixer
+import SQL_fitopsy
+
+venster = Tk()
+
+#--------opstarten---------------------#
+venster.wm_title("fitopsy" )
+venster["bg"] = "white"
+venster.iconbitmap("fitopsy.ico")
+
+
+
+
+###------------------Functie defenities-----------------------------
+def zoekKlant():
+    gevonden_klanten=SQL_fitopsy.zoekKlantInTabel(ingevoerde_klantnaam.get())
+    print(gevonden_klanten)
+
+###------------------Hoofdprogramma---------------------------------
+labelIntro = Label(venster,text="welkom" )
+labelIntro.grid(row=0, column=0, sticky="W")
+
+labelklantnaam = Label(venster,text="klantnaam:" )
+labelklantnaam.grid(row=1, column=0, sticky="W")
+
+ingevoerde_klantnaam = StringVar()
+entryKlantnaam = Entry(venster, textvariable=ingevoerde_klantnaam)
+entryKlantnaam.grid(row=1, column=1, sticky="w")
+
+knopzoekopklant = Button(venster, text="zoek klant", width= 12, command=zoekKlant)
+knopzoekopklant.grid(row=1,column=4,sticky="W")
+
+knopSluit = Button(venster, text="Sluiten", width=12, command=venster.destroy)
+knopSluit.grid(row=17, column=4)
+
+venster.mainloop()
+
