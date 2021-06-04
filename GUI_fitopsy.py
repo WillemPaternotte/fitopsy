533c4bac8c2d92707d4555a5130e07c45a611342
Robin-Kuijpers <74715208+Robin-Kuijpers@users.noreply.github.com>
2021-06-04 22:36:49 +0200

    wat comments en window grootte

--- GUI_fitopsy.py
+++ GUI_fitopsy.py
@@ -16,6 +16,7 @@ import SQL_fitopsy
 venster = Tk()
 
 #--------opstarten---------------------#
+venster.geometry("600x600") #bepaalt grootte window
 mainFont = Font(
     family="Comic Sans MS",
     size= 40,
@@ -25,18 +26,16 @@ venster["bg"] = "white"
 venster.iconbitmap("fitopsy.ico")
 
 
-
-
 ###------------------Functie defenities-----------------------------
 def zoekNummer():
     gevonden_nummer=SQL_fitopsy.getSongFromTable("songs", ingevoerde_nummer.get())
     print(gevonden_nummer)
 
 
-def BestandKiezen():
+def BestandKiezen(): #zorgt ervoor dat windows verkenner opent, zodat filepath niet handmatigf moet worden ingevuld
     file_path = filedialog.askopenfilenames()
 
-    print(file_path)
+    print(file_path) 
 
 def NummerToevoegen():
     nieuw_nummer=SQL_fitopsy.addSong()
@@ -47,30 +46,30 @@ def NummerToevoegen():
 
 
 ###------------------Hoofdprogramma---------------------------------
-labelIntro = Label(venster,bg = "white", text="F I T O P S Y", font = mainFont )
+labelIntro = Label(venster,bg = "white", text="F I T O P S Y", font = mainFont ) #titel
 labelIntro.grid(row=0,column=0, sticky="w")
 
-labelNummer = Label(venster,bg ="White", text="nummer:" )
+labelNummer = Label(venster,bg ="White", text="nummer:" ) #nummerzoeken
 labelNummer.grid(row=1, column=0, sticky="W")
 
 ingevoerde_nummer = StringVar()
-entryNummer = Entry(venster, textvariable=ingevoerde_nummer)
+entryNummer = Entry(venster, textvariable=ingevoerde_nummer) #nummerzoeken
 entryNummer.grid(row=1, column=2, sticky="W")
 
-knopNummer = Button(venster, text="zoek nummer", width= 14, command=zoekNummer)
+knopNummer = Button(venster, text="zoek nummer", width= 14, command=zoekNummer) #nummerzoeken
 knopNummer.grid(row=1, column=3, sticky="W")
 
-LabelNummerToevoegen = Label(venster,bg="white", text=" Nummer toevoegen:")
+LabelNummerToevoegen = Label(venster,bg="white", text=" Nummer toevoegen:") #nummertoevoegen
 LabelNummerToevoegen.grid(row=2, column=0, sticky="W")
 
 NieuwNummer = StringVar()
-entryNummerToevoegen = Entry(venster, textvariable=NieuwNummer)
+entryNummerToevoegen = Entry(venster, textvariable=NieuwNummer) #nummertoevoegen
 entryNummerToevoegen.grid(row=2, column=2, sticky="W")
 
-knopNummerToevoegen = Button(venster,text="voeg nummer toe", width=14, command=NummerToevoegen)
+knopNummerToevoegen = Button(venster,text="voeg nummer toe", width=14, command=NummerToevoegen) #nummertoevoegen
 knopNummerToevoegen.grid(row=2, column=3, sticky="W")
 
-knopBestand = Button(venster, text="zoek bestand", width= 14, command=BestandKiezen)
+knopBestand = Button(venster, text="zoek bestand", width= 14, command=BestandKiezen) #bestand kiezen
 knopBestand.grid(row=3, column=3, sticky="W")
 
 # place(relx=0.5, rely=0.5, anchor=CENTER)
